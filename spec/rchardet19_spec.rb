1d22fa9ec49467c123ba9d40a7f41367bf7ffc87
Linus Oleander <linus@oleander.nu>
2011-02-16 23:08:56 +0100

    Adding some empty tests

---
+++ spec/rchardet19_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe CharDet do
+  it "should work" do
+    true.should be_true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require 'rchardet19'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
